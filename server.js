7cb2d37cf3e3233e6a1953ea24d96518e748eac4
Julio Fuentes <juliodddddd1234@gmail.com>
2022-06-20 16:19:42 -0600

    socket.io instance has been created

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const port = 5000;
 const cors = require("cors");
 const passport = require("passport");
-// const { localStrategyF } = require("./strategies/local");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
-const { JwtStrategy } = require("./strategies/jwt")
 const { catchError, handleError } = require("./middleware/error.handler");
 const cookieparser = require("cookie-parser");
 //SocketServer
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const socket = require("./socket/socket");
-const Feedback = require("./model/message");
-const { Strategy, ExtractJwt } = require('passport-jwt');
-const config = require('./config/config');
-
+const Sockets = require('./socket/socket');
 
 // const { JwtStrategy } = require('./strategies/jwt')
 const app = express();
@@ -102,86 +95,7 @@ const io = new Server(httpServer, {
 	}
 });
 
-
-//test
-
-// const options1 = {
-// 	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), //De donde va a sacar el token
-// 	secretOrKey: config.jwtSecret
-// };
-
-// passport.use(new Strategy(options1, (payload, done) => {
-// 	// console.log(payload)
-// 	// return done(null, payload)
-// 	console.log('Payload', payload)
-// 	User.findOne({ id: payload.id }, function (err, user) {
-// 		if (err) {
-// 			return done(err, false);
-// 		}
-// 		if (user) {
-// 			return done(null, user);
-// 		} else {
-// 			return done(null, false);
-// 			// or you could create a new account
-// 		}
-// 	});
-// }));
-
-// passport.serializeUser(function (user, done) {
-// 	if (user) done(null, user)
-// })
-
-// passport.deserializeUser(function (id, done) {
-// 	done(null, id)
-// })
-
-// const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
-const wrap = middleware => (socket, next) => middleware(socket.request, {}, next)
-// io.use(wrap(passport.initialize()));
-// io.use(wrap(passport.session()))
-io.use(wrap(passport.authenticate('jwt', { session: false })));
-
-io.use((socket, next) => {
-	console.log(socket.header)
-	next()
-	// if (socket.request.user) {
-	// 	next();
-	// } else {
-	// 	console.log("Error socket")
-	// 	next(new Error("Unauthorized"))
-	// }
-});
-
-io.on("connection", (socket) => {
-	console.log(`User connected ${socket.id}`)
-	// const all = await Feedback.find();
-	socket.on("get", async () => {
-		const all = await Feedback.find();
-		socket.emit("getFeed", all)
-	})
-	// console.log(socket)
-
-	socket.on("test", (data) => {
-		console.log(data.message)
-	})
-
-	socket.on("addFeedback", async (data) => {
-		console.log(data)
-		const newFeedback = new Feedback({
-			title: data.title,
-			feature: data.feature,
-			description: data.description,
-			// test: test
-		});
-		//Guarda la base de datos
-		await newFeedback.save();
-		const all = await Feedback.find({});
-		// console.log('All', all)
-		io.emit("update", all)
-	})
-})
-
-// socket(io)
+Sockets(io);
 
 httpServer.listen(port, () => {
 	console.log('Server is ready in the port 8000')
--- socket/socket.js
+++ socket/socket.js
@@ -1,22 +1,52 @@
-
+const Feedback = require("../model/message");
+const passport = require('passport');
 
 const socket = (io) => {
-    io.on('connection', (socket) => {
-        console.log('User connected paaa', socket.id);
-        console.log('User connected paaa', socket.connected);
+    const wrap = middleware => (socket, next) => middleware(socket.request, {}, next)
+    // io.use(wrap(passport.initialize()));
+    // io.use(wrap(passport.session()))
+    io.use(wrap(passport.authenticate('jwt', { session: false })));
 
+    io.use((socket, next) => {
+        console.log(socket.header)
+        // next()
+        if (socket.request.user) {
+            next();
+        } else {
+            console.log("Error socket")
+            next(new Error("Unauthorized"))
+        }
+    });
 
-        socket.on('test', (name) => {
-            console.log(`El usuario del cliente ${name}`)
+    io.on("connection", (socket) => {
+        console.log(`User connected ${socket.id}`)
+        // const all = await Feedback.find();
+        socket.on("get", async () => {
+            const all = await Feedback.find();
+            socket.emit("getFeed", all)
+        })
+        // console.log(socket)
+
+        socket.on("test", (data) => {
+            console.log(data.message)
         })
 
-        socket.on('addUpvote', (callback) => {
-            console.log('El socket addUpvote esta funcionando...')
-            socket.emit('client')
+        socket.on("addFeedback", async (data) => {
+            console.log(data)
+            const newFeedback = new Feedback({
+                title: data.title,
+                feature: data.feature,
+                description: data.description,
+                // test: test
+            });
+            //Guarda la base de datos
+            await newFeedback.save();
+            const all = await Feedback.find({});
+            // console.log('All', all)
+            io.emit("update", all)
         })
-
     })
 
 }
 
-module.exports = socket
+module.exports = socket;
