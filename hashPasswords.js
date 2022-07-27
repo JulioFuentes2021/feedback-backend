2e4b55ba802534d6806040490c111c062000e787
Julio Fuentes <juliodddddd1234@gmail.com>
2022-07-27 15:11:15 -0600

    Test files were removed

--- hashPasswords.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const hash = async () => {
-    const myPassword = "admin123";
-    const hash = await bcrypt.hash(myPassword, 10);
-    console.log(hash);
-}
-
-hash()
